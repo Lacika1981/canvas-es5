611ea53b2d60946d251e12ce6baa399676149869
Laszlo Varga <vargalaszlo1981@googlemail.com>
2017-06-17 19:49:21 +0100

    adding the files to the repo and updating the README.md file

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # canvas-es5
+
+Hi,
+
+This is my repository to the Canvas lessons at Udacity. This is only for experimental sake and there is not much meaning. If you look at my code and think something is wrong or could be done better then please do not hesitate to inform me. 
+Here is the [link](https://github.com/Lacika1981/canvas) to my repo where I used closures to solve this puzzle.
+
+Thank you
---
+++ canvas.js
@@ -0,0 +1,52 @@
+
+var c = document.querySelector("#c");
+var ctx = c.getContext("2d");
+
+
+var Path = function (x, y, width, height, ctx) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.ctx = ctx;
+}
+
+var Rectangle = function (x, y, width, height, ctx) {
+    Path.apply(this, arguments);
+    this.drawRect(x, y, width, height, ctx);
+}
+
+Rectangle.prototype = Object.create(Path.prototype);
+Rectangle.prototype.constructor = Rectangle;
+
+Rectangle.prototype.drawRect = function (x, y, width, height, ctx) {
+    ctx.beginPath();
+    ctx.lineTo(x, y);
+    ctx.lineTo(x = x + width, y);
+    ctx.lineTo(x, y = y + height);
+    ctx.lineTo(x = x - width, y);
+    ctx.strokeStyle = "black";
+    ctx.closePath();
+    ctx.stroke();
+}
+
+var Triangle = function (x, y, width, height, ctx) {
+    Path.apply(this, arguments);
+    this.drawTriangle(x, y, width, height, ctx);
+}
+
+Triangle.prototype = Object.create(Path.prototype);
+Triangle.prototype.constructor = Triangle;
+
+Triangle.prototype.drawTriangle = function (x, y, width, height, ctx) {
+    ctx.beginPath();
+    ctx.lineTo(x, y);
+    ctx.lineTo(x = x + width, y);
+    ctx.lineTo(x, y = y + height);
+    ctx.fillStyle = "green";
+    ctx.closePath();
+    ctx.fill();
+}
+
+var rect = new Rectangle(200, 200, 200, 200, ctx);
+var tri = new Triangle(250, 250, 100, 100, ctx);
